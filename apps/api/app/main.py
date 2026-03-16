dee6dd857b5d961d0f9ae43b9f41fba99c538542
KonstantinVajra <kmkm27@mail.ru>
2026-03-16 21:40:38 +0300

    fix: add 3001 frontend origin to CORS

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -26,6 +26,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://213.176.16.155:3000",
+         "http://213.176.16.155:3001",
     ],
     allow_credentials=True,
     allow_methods=["*"],
